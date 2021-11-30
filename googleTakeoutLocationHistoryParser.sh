006cad72d2b6b16022a7371347e402c6bef877c4
Chris Beley <chris.beley@gmail.com>
2021-11-29 23:39:57 -0800

    Ensure executable can find the node file.

--- googleTakeoutLocationHistoryParser.sh
+++ googleTakeoutLocationHistoryParser.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-/usr/bin/env node index.mjs "$@"
+# Special thanks to https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+/usr/bin/env node $SCRIPT_DIR/index.mjs "$@"
